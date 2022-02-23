95e914ea8f7aa208452b8391b17f68a3cc1d8724
Amy Ariel <aariel3145@gmail.com>
2022-02-22 19:56:09 -0500

    Actually commited David ITH

--- david-ITH.py
+++ david-ITH.py
@@ -1,7 +1,8 @@
 from datetime import date
 
-day0 = date(2022, 2, 3)
+day0 = date(2021, 6, 11)
+end = date(2022, 2, 16)
 today = date.today()
 
 print("Days since David has failed to watch In the Heights: ", end = "")
-print(f"{(today - day0).days}")
+print(f"{(end - day0).days}")
